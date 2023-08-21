0f618ee12c763b05b3892996151d7e37d9d0087b
nyxb <contact@nyxb.xyz>
2023-08-21 22:12:25 +0200

    🐛 fix(has-permissions.ts): fix indentation and add type annotations for better code readability ✨ feat(has-permissions.ts): improve error handling and messaging when user is missing required permissions

--- src/command-handler/validations/runtime/has-permissions.ts
+++ src/command-handler/validations/runtime/has-permissions.ts
@@ -1,65 +1,63 @@
 import { PermissionFlagsBits } from 'discord.js'
 
 import requiredPermissions from '../../../models/required-permissions-schema'
-import Command from '../../Command'
-import { CommandUsage } from '../../../../typings'
+import type Command from '../../Command'
+import type { CommandUsage } from '../../../../typings'
 
 const keys = Object.keys(PermissionFlagsBits)
 
 export default async (command: Command, usage: CommandUsage) => {
-  const { permissions = [] } = command.commandObject
-  const { instance, guild, member, message, interaction } = usage
+   const { permissions = [] } = command.commandObject
+   const { instance, guild, member, message, interaction } = usage
 
-  if (!member) {
-    return true
-  }
+   if (!member)
+      return true
 
-  if (instance.isConnectedToDB) {
-    const document = await requiredPermissions.findById(
-      `${guild!.id}-${command.commandName}`
-    )
-    if (document) {
-      for (const permission of document.permissions) {
-        if (!permissions.includes(permission)) {
-          permissions.push(permission)
-        }
+   if (instance.isConnectedToDB) {
+      const document = await requiredPermissions.findById(
+      `${guild!.id}-${command.commandName}`,
+      )
+      if (document) {
+         for (const permission of document.permissions) {
+            if (!permissions.includes(permission))
+               permissions.push(permission)
+         }
       }
-    }
-  }
+   }
 
-  if (permissions.length) {
-    const missingPermissions = []
+   if (permissions.length) {
+      const missingPermissions = []
 
-    for (const permission of permissions) {
-      // @ts-ignore
-      if (!member.permissions.has(permission)) {
-        const permissionName = keys.find(
-          // @ts-ignore
-          (key) => key === permission || PermissionFlagsBits[key] === permission
-        )
-        missingPermissions.push(permissionName)
+      for (const permission of permissions) {
+         if (!member.permissions.has(permission)) {
+            const permissionName = keys.find(
+               // @ts-expect-error is fine
+               key => key === permission || PermissionFlagsBits[key] === permission,
+            )
+            missingPermissions.push(permissionName)
+         }
       }
-    }
 
-    if (missingPermissions.length) {
-      const content = `You are missing the following permissions: "${missingPermissions.join(
-        '", "'
+      if (missingPermissions.length) {
+         const content = `You are missing the following permissions: "${missingPermissions.join(
+        '", "',
       )}"`
 
-      if (message) {
-        message.reply({
-          content,
-        })
-      } else if (interaction) {
-        interaction.reply({
-          content,
-          ephemeral: true,
-        })
+         if (message) {
+            message.reply({
+               content,
+            })
+         }
+         else if (interaction) {
+            interaction.reply({
+               content,
+               ephemeral: true,
+            })
+         }
+
+         return false
       }
+   }
 
-      return false
-    }
-  }
-
-  return true
+   return true
 }
