eb6e903dfaa2156272774f1783c85c1640463b4a
nyxb <contact@nyxb.xyz>
2023-08-21 22:16:26 +0200

    🐛 fix(test-without-server.ts): remove unnecessary semicolons and fix indentation for better code readability ✨ feat(EventHandler.ts): change import statements to use type imports for better type checking and remove unused imports 🐛 fix(EventHandler.ts): fix indentation and remove unnecessary semicolons for better code readability ✨ feat(EventHandler.ts): add support for dynamic validation functions in event callbacks 🐛 fix(slash-commands.ts): remove unnecessary semicolons and fix indentation for better code readability

--- src/event-handler/events/interactionCreate/isCommand/slash-commands.ts
+++ src/event-handler/events/interactionCreate/isCommand/slash-commands.ts
@@ -1,46 +1,44 @@
-import { CommandInteraction } from "discord.js";
+import type { CommandInteraction } from 'discord.js'
 
-import NYXB from "../../../../../typings";
+import type NYXB from '../../../../../typings'
 
 export default async (interaction: CommandInteraction, instance: NYXB) => {
-  const { commandHandler } = instance;
-  if (!commandHandler) {
-    return;
-  }
+   const { commandHandler } = instance
+   if (!commandHandler)
+      return
 
-  const { commands, customCommands } = commandHandler;
+   const { commands, customCommands } = commandHandler
 
-  const args = interaction.options.data.map(({ value }) => {
-    return String(value);
-  });
+   const args = interaction.options.data.map(({ value }) => {
+      return String(value)
+   })
 
-  const command = commands.get(interaction.commandName);
-  if (!command) {
-    customCommands.run(interaction.commandName, null, interaction);
-    return;
-  }
+   const command = commands.get(interaction.commandName)
+   if (!command) {
+      customCommands.run(interaction.commandName, null, interaction)
+      return
+   }
 
-  const { deferReply } = command.commandObject;
+   const { deferReply } = command.commandObject
 
-  if (deferReply) {
-    await interaction.deferReply({
-      ephemeral: deferReply === "ephemeral",
-    });
-  }
+   if (deferReply) {
+      await interaction.deferReply({
+         ephemeral: deferReply === 'ephemeral',
+      })
+   }
 
-  const response = await commandHandler.runCommand(
-    command,
-    args,
-    null,
-    interaction
-  );
-  if (!response) {
-    return;
-  }
+   const response = await commandHandler.runCommand(
+      command,
+      args,
+      null,
+      interaction,
+   )
+   if (!response)
+      return
 
-  if (deferReply) {
-    interaction.editReply(response).catch(() => {});
-  } else {
-    interaction.reply(response).catch(() => {});
-  }
-};
+   if (deferReply)
+      interaction.editReply(response).catch(() => {})
+
+   else
+      interaction.reply(response).catch(() => {})
+}
