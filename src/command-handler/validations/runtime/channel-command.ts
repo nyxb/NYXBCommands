a1ecaa21962eb5f380640a4a3f89998ee51c85b3
nyxb <contact@nyxb.xyz>
2023-08-21 22:11:35 +0200

    🐛 fix(channel-command.ts): fix indentation and add type annotations for better code readability ✨ feat(channel-command.ts): improve error handling by replying with appropriate messages when command is not allowed in the current channel

--- src/command-handler/validations/runtime/channel-command.ts
+++ src/command-handler/validations/runtime/channel-command.ts
@@ -1,32 +1,31 @@
-import Command from '../../Command'
-import { CommandUsage } from '../../../../typings'
+import type Command from '../../Command'
+import type { CommandUsage } from '../../../../typings'
 
 export default async (command: Command, usage: CommandUsage) => {
-  const { commandName, instance } = command
-  const { guild, channel, message, interaction } = usage
+   const { commandName, instance } = command
+   const { guild, channel, message, interaction } = usage
 
-  if (!guild || !instance.isConnectedToDB) {
-    return true
-  }
+   if (!guild || !instance.isConnectedToDB)
+      return true
 
-  const availableChannels =
-    await instance.commandHandler.channelCommands.getAvailableChannels(
-      guild.id,
-      commandName
+   const availableChannels
+    = await instance.commandHandler.channelCommands.getAvailableChannels(
+       guild.id,
+       commandName,
     )
 
-  if (availableChannels.length && !availableChannels.includes(channel!.id)) {
-    const channelNames = availableChannels.map((c: string) => `<#${c}> `)
-    const content = `You can only run this command inside of the following channels: ${channelNames}.`
+   if (availableChannels.length && !availableChannels.includes(channel!.id)) {
+      const channelNames = availableChannels.map((c: string) => `<#${c}> `)
+      const content = `You can only run this command inside of the following channels: ${channelNames}.`
 
-    if (message) {
-      message.reply({ content })
-    } else if (interaction) {
-      interaction.reply({ content, ephemeral: true })
-    }
+      if (message)
+         message.reply({ content })
 
-    return false
-  }
+      else if (interaction)
+         interaction.reply({ content, ephemeral: true })
 
-  return true
+      return false
+   }
+
+   return true
 }
