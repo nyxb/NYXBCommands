cd5a4bcdd2e4a46310e3f175e079275933dc56c6
nyxb <contact@nyxb.xyz>
2023-08-21 22:09:25 +0200

    🔧 fix(SlashCommands.ts): fix import statements and formatting for better readability ✨ feat(SlashCommands.ts): add support for fetching and creating slash commands in Discord.js 🔧 fix(SlashCommands.ts): fix comparison logic in areOptionsDifferent method ✨ feat(SlashCommands.ts): add support for updating and deleting slash commands in Discord.js ✨ feat(SlashCommands.ts): add createOptions method to generate command options based on expectedArgs and minArgs

--- src/command-handler/SlashCommands.ts
+++ src/command-handler/SlashCommands.ts
@@ -1,150 +1,154 @@
+import type {
+   ApplicationCommandOption,
+   Client,
+} from 'discord.js'
 import {
-  ApplicationCommandOption,
-  ApplicationCommandOptionType,
-  Client,
-} from "discord.js";
+   ApplicationCommandOptionType,
+} from 'discord.js'
 
 class SlashCommands {
-  private _client: Client;
+   private _client: Client
 
-  constructor(client: Client) {
-    this._client = client;
-  }
+   constructor(client: Client) {
+      this._client = client
+   }
 
-  async getCommands(guildId?: string) {
-    let commands;
+   async getCommands(guildId?: string) {
+      let commands
 
-    if (guildId) {
-      const guild = await this._client.guilds.fetch(guildId);
-      commands = guild.commands;
-    } else {
-      commands = this._client.application?.commands;
-    }
+      if (guildId) {
+         const guild = await this._client.guilds.fetch(guildId)
+         commands = guild.commands
+      }
+      else {
+         commands = this._client.application?.commands
+      }
 
-    // @ts-ignore
-    await commands?.fetch();
+      // @ts-expect-error is fine
+      await commands?.fetch()
 
-    return commands;
-  }
+      return commands
+   }
 
-  areOptionsDifferent(
-    options: ApplicationCommandOption[], 
-    existingOptions: any
-  ) {
-    if (options.length !== existingOptions.length) return true;
-    let different = false;
-    for (let a = 0; a < existingOptions.length; ++a) {
-        const option: any = options[a];
-        const existing = existingOptions[a];
-        Object.entries(existing).forEach(([key, value]) => {
-            const existingElement = existing[key] ? existing[key] : false;
-            const optionElement = option[key] ? option[key] : false;
+   areOptionsDifferent(
+      options: ApplicationCommandOption[],
+      existingOptions: any,
+   ) {
+      if (options.length !== existingOptions.length)
+         return true
+      let different = false
+      for (let a = 0; a < existingOptions.length; ++a) {
+         const option: any = options[a]
+         const existing = existingOptions[a]
+         Object.entries(existing).forEach(([key, value]) => {
+            const existingElement = existing[key] ? existing[key] : false
+            const optionElement = option[key] ? option[key] : false
 
             if (
-                key == "options" &&
-                existingElement !== false &&
-                optionElement !== false
+               key === 'options'
+                && existingElement !== false
+                && optionElement !== false
             ) {
-                for (let i = 0; i < existing.options.length; ++i) {
-                    const existingOption = existing.options[i];
-                    const addOption = option.options[i];
-                    if (
-                        JSON.stringify(existingOption) === JSON.stringify(addOption)
-                    ) {
-                        different = true;
-                    }
-                }
-            } else if (
-                String(existingElement) !== String(optionElement)
-            ) {
-                different = true;
+               for (let i = 0; i < existing.options.length; ++i) {
+                  const existingOption = existing.options[i]
+                  const addOption = option.options[i]
+                  if (
+                     JSON.stringify(existingOption) === JSON.stringify(addOption)
+                  )
+                     different = true
+               }
             }
-        })
-        continue;
-    }
-                                                            
-    return different;
-  }
-
-  async create(
-    name: string,
-    description: string,
-    options: ApplicationCommandOption[],
-    guildId?: string
-  ) {
-    const commands = await this.getCommands(guildId);
-    if (!commands) {
-      throw new Error(`Could not find commands for guild ${guildId}`);
-    }
-
-    const existingCommand = commands.cache.find((cmd) => cmd.name === name);
-    if (existingCommand) {
-      const { description: existingDescription, options: existingOptions } =
-        existingCommand;
-
-      if (
-        description !== existingDescription ||
-        this.areOptionsDifferent(options, existingOptions)
-      ) {
-        console.log(`Updating the command "${name}"`);
-
-        await commands.edit(existingCommand.id, {
-          description,
-          options,
-        });
+            else if (
+               String(existingElement) !== String(optionElement)
+            ) {
+               different = true
+            }
+         })
+         continue
       }
-      return;
-    }
 
-    await commands.create({
-      name,
-      description,
-      options,
-    });
-  }
+      return different
+   }
 
-  async delete(commandName: string, guildId?: string) {
-    const commands = await this.getCommands(guildId);
+   async create(
+      name: string,
+      description: string,
+      options: ApplicationCommandOption[],
+      guildId?: string,
+   ) {
+      const commands = await this.getCommands(guildId)
+      if (!commands)
+         throw new Error(`Could not find commands for guild ${guildId}`)
 
-    const existingCommand = commands?.cache.find(
-      (cmd) => cmd.name === commandName
-    );
-    if (!existingCommand) {
-      return;
-    }
+      const existingCommand = commands.cache.find(cmd => cmd.name === name)
+      if (existingCommand) {
+         const { description: existingDescription, options: existingOptions }
+        = existingCommand
 
-    await existingCommand.delete();
-  }
+         if (
+            description !== existingDescription
+        || this.areOptionsDifferent(options!, existingOptions)
+         ) {
+            console.log(`Updating the command "${name}"`)
 
-  createOptions({
-    expectedArgs = "",
+            await commands.edit(existingCommand.id, {
+               description,
+               // @ts-expect-error is fine
+               options,
+            })
+         }
+         return
+      }
+
+      await commands.create({
+         name,
+         description,
+         // @ts-expect-error is fine
+         options,
+      })
+   }
+
+   async delete(commandName: string, guildId?: string) {
+      const commands = await this.getCommands(guildId)
+
+      const existingCommand = commands?.cache.find(
+         cmd => cmd.name === commandName,
+      )
+      if (!existingCommand)
+         return
+
+      await existingCommand.delete()
+   }
+
+   createOptions({
+      expectedArgs = '',
     minArgs = 0,
-  }): ApplicationCommandOption[] {
-    const options: ApplicationCommandOption[] = [];
+   }): ApplicationCommandOption[] {
+      const options: ApplicationCommandOption[] = []
 
-    // <num 1> <num 2>
+      // <num 1> <num 2>
 
-    if (expectedArgs) {
-      const split = expectedArgs
-        .substring(1, expectedArgs.length - 1)
-        // num 1> <num 2
-        .split(/[>\]] [<\[]/);
-      // ['num 1', 'num 2']
+      if (expectedArgs) {
+         const split = expectedArgs
+            .substring(1, expectedArgs.length - 1)
+         // num 1> <num 2
+            .split(/[>\]] [<\[]/)
+         // ['num 1', 'num 2']
 
-      for (let a = 0; a < split.length; ++a) {
-        const arg = split[a];
+         for (let a = 0; a < split.length; ++a) {
+            const arg = split[a]
 
-        options.push({
-          name: arg.toLowerCase().replace(/\s+/g, "-"),
-          description: arg,
-          type: ApplicationCommandOptionType.String,
-          required: a < minArgs,
-        });
+            options.push({
+               name: arg.toLowerCase().replace(/\s+/g, '-'),
+               description: arg,
+               type: ApplicationCommandOptionType.String,
+               required: a < minArgs,
+            })
+         }
       }
-    }
 
-    return options;
-  }
+      return options
+   }
 }
 
-export default SlashCommands;
+export default SlashCommands
