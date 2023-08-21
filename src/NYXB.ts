b12b93ac0311277f2a10150fb11e2b6e48d95fc7
nyxb <contact@nyxb.xyz>
2023-08-21 22:08:11 +0200

    🔨 refactor(NYXB.ts): organize imports and fix indentation for better readability 🔨 refactor(index.ts): organize imports and fix indentation for better readability

--- src/NYXB.ts
+++ src/NYXB.ts
@@ -1,144 +1,140 @@
-import { Client } from 'discord.js'
+import type { Client } from 'discord.js'
 import mongoose from 'mongoose'
 
+import type { Events, Options, Validations } from '../typings'
+import type NYXB from '../typings'
 import CommandHandler from './command-handler/CommandHandler'
 import EventHandler from './event-handler/EventHandler'
-import NYXB, { Events, Options, Validations } from '../typings'
 import Cooldowns from './util/Cooldowns'
-import DefaultCommands from './util/DefaultCommands'
+import type DefaultCommands from './util/DefaultCommands'
 import FeaturesHandler from './util/FeaturesHandler'
 
 class NYXBCommands {
-  private _client!: Client
-  private _testServers!: string[]
-  private _botOwners!: string[]
-  private _cooldowns: Cooldowns | undefined
-  private _disabledDefaultCommands!: DefaultCommands[]
-  private _validations!: Validations
-  private _commandHandler: CommandHandler | undefined
-  private _eventHandler!: EventHandler
-  private _isConnectedToDB = false
-  private _defaultPrefix = '!'
+   private _client!: Client
+   private _testServers!: string[]
+   private _botOwners!: string[]
+   private _cooldowns: Cooldowns | undefined
+   private _disabledDefaultCommands!: DefaultCommands[]
+   private _validations!: Validations
+   private _commandHandler: CommandHandler | undefined
+   private _eventHandler!: EventHandler
+   private _isConnectedToDB = false
+   private _defaultPrefix = '!'
 
-  constructor(options: Options) {
-    this.init(options)
-  }
+   constructor(options: Options) {
+      this.init(options)
+   }
 
-  private async init(options: Options) {
-    let {
-      client,
-      mongoUri,
-      commandsDir,
-      featuresDir,
-      testServers = [],
-      botOwners = [],
-      cooldownConfig,
-      disabledDefaultCommands = [],
-      events = {},
-      validations = {},
-      defaultPrefix,
-    } = options
+   private async init(options: Options) {
+      const {
+         client,
+         mongoUri,
+         commandsDir,
+         featuresDir,
+         testServers = [],
+         botOwners = [],
+         cooldownConfig,
+         disabledDefaultCommands = [],
+         events = {},
+         validations = {},
+         defaultPrefix,
+      } = options
 
-    if (!client) {
-      throw new Error('A client is required.')
-    }
+      if (!client)
+         throw new Error('A client is required.')
 
-    if (mongoUri) {
-      await this.connectToMongo(mongoUri)
-    }
+      if (mongoUri)
+         await this.connectToMongo(mongoUri)
 
-    // Add the bot owner's ID
-    if (botOwners.length === 0) {
-      await client.application?.fetch()
-      const ownerId = client.application?.owner?.id
-      if (ownerId && botOwners.indexOf(ownerId) === -1) {
-        botOwners.push(ownerId)
+      // Add the bot owner's ID
+      if (botOwners.length === 0) {
+         await client.application?.fetch()
+         const ownerId = client.application?.owner?.id
+         if (ownerId && !botOwners.includes(ownerId))
+            botOwners.push(ownerId)
       }
-    }
 
-    this._client = client
-    this._testServers = testServers
-    this._botOwners = botOwners
-    this._disabledDefaultCommands = disabledDefaultCommands
-    this._validations = validations
+      this._client = client
+      this._testServers = testServers
+      this._botOwners = botOwners
+      this._disabledDefaultCommands = disabledDefaultCommands
+      this._validations = validations
 
-    this._cooldowns = new Cooldowns(this as unknown as NYXB, {
-      errorMessage: 'Please wait {TIME} before doing that again.',
-      botOwnersBypass: false,
-      dbRequired: 300, // 5 minutes
-      ...cooldownConfig,
-    })
+      this._cooldowns = new Cooldowns(this as unknown as NYXB, {
+         errorMessage: 'Please wait {TIME} before doing that again.',
+         botOwnersBypass: false,
+         dbRequired: 300, // 5 minutes
+         ...cooldownConfig,
+      })
 
-    if (defaultPrefix) {
-      this._defaultPrefix = defaultPrefix
-    }
+      if (defaultPrefix)
+         this._defaultPrefix = defaultPrefix
 
-    if (commandsDir) {
-      this._commandHandler = new CommandHandler(
-        this as unknown as NYXB,
-        commandsDir,
-        client
+      if (commandsDir) {
+         this._commandHandler = new CommandHandler(
+            this as unknown as NYXB,
+            commandsDir,
+            client,
+         )
+      }
+
+      if (featuresDir)
+         new FeaturesHandler(this as unknown as NYXB, featuresDir, client)
+
+      this._eventHandler = new EventHandler(
+         this as unknown as NYXB,
+         events as Events,
+         client,
       )
-    }
+   }
 
-    if (featuresDir) {
-      new FeaturesHandler(this as unknown as NYXB, featuresDir, client)
-    }
+   public get client(): Client {
+      return this._client
+   }
 
-    this._eventHandler = new EventHandler(
-      this as unknown as NYXB,
-      events as Events,
-      client
-    )
-  }
+   public get testServers(): string[] {
+      return this._testServers
+   }
 
-  public get client(): Client {
-    return this._client
-  }
+   public get botOwners(): string[] {
+      return this._botOwners
+   }
 
-  public get testServers(): string[] {
-    return this._testServers
-  }
+   public get cooldowns(): Cooldowns | undefined {
+      return this._cooldowns
+   }
 
-  public get botOwners(): string[] {
-    return this._botOwners
-  }
+   public get disabledDefaultCommands(): DefaultCommands[] {
+      return this._disabledDefaultCommands
+   }
 
-  public get cooldowns(): Cooldowns | undefined {
-    return this._cooldowns
-  }
+   public get commandHandler(): CommandHandler | undefined {
+      return this._commandHandler
+   }
 
-  public get disabledDefaultCommands(): DefaultCommands[] {
-    return this._disabledDefaultCommands
-  }
+   public get eventHandler(): EventHandler {
+      return this._eventHandler
+   }
 
-  public get commandHandler(): CommandHandler | undefined {
-    return this._commandHandler
-  }
+   public get validations(): Validations {
+      return this._validations
+   }
 
-  public get eventHandler(): EventHandler {
-    return this._eventHandler
-  }
+   public get isConnectedToDB(): boolean {
+      return this._isConnectedToDB
+   }
 
-  public get validations(): Validations {
-    return this._validations
-  }
+   public get defaultPrefix(): string {
+      return this._defaultPrefix
+   }
 
-  public get isConnectedToDB(): boolean {
-    return this._isConnectedToDB
-  }
+   private async connectToMongo(mongoUri: string) {
+      await mongoose.connect(mongoUri, {
+         keepAlive: true,
+      })
 
-  public get defaultPrefix(): string {
-    return this._defaultPrefix
-  }
-
-  private async connectToMongo(mongoUri: string) {
-    await mongoose.connect(mongoUri, {
-      keepAlive: true,
-    })
-
-    this._isConnectedToDB = true
-  }
+      this._isConnectedToDB = true
+   }
 }
 
 export default NYXBCommands
