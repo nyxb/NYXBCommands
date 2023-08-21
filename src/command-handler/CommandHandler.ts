5944da73e21008ffb719c20e20984a81accac51c
nyxb <contact@nyxb.xyz>
2023-08-21 22:09:02 +0200

    —————————————————— 🔧 fix(CustomCommands.ts): change import statements to use type imports to improve type safety ✨ feat(CustomCommands.ts): add type annotations to class properties and method parameters for better code readability and maintainability

--- src/command-handler/CommandHandler.ts
+++ src/command-handler/CommandHandler.ts
@@ -1,249 +1,246 @@
-import {
-  ChatInputCommandInteraction,
-  Client,
-  GuildMember,
-  Message,
-  TextChannel,
+import path from 'node:path'
+import type {
+   ChatInputCommandInteraction,
+   Client,
+   GuildMember,
+   Message,
+   TextChannel,
 } from 'discord.js'
-import path from 'path'
 
 import getAllFiles from '../util/get-all-files'
+import CommandType from '../util/CommandType'
+import type {
+   CommandObject,
+   CommandUsage,
+   InternalCooldownConfig,
+} from '../../typings'
+import type NYXB from '../../typings'
+import DefaultCommands from '../util/DefaultCommands'
 import Command from './Command'
 import SlashCommands from './SlashCommands'
 import ChannelCommands from './ChannelCommands'
 import CustomCommands from './CustomCommands'
 import DisabledCommands from './DisabledCommands'
 import PrefixHandler from './PrefixHandler'
-import CommandType from '../util/CommandType'
-import NYXB, {
-  CommandObject,
-  CommandUsage,
-  InternalCooldownConfig,
-} from '../../typings'
-import DefaultCommands from '../util/DefaultCommands'
 
 class CommandHandler {
-  // <commandName, instance of the Command class>
-  private _commands: Map<string, Command> = new Map()
-  private _validations = this.getValidations(
-    path.join(__dirname, 'validations', 'runtime')
-  )
-  private _instance: NYXB
-  private _client: Client
-  private _commandsDir: string
-  private _slashCommands: SlashCommands
-  private _channelCommands: ChannelCommands
-  private _customCommands: CustomCommands
-  private _disabledCommands: DisabledCommands
-  private _prefixes: PrefixHandler
+   // <commandName, instance of the Command class>
+   private _commands: Map<string, Command> = new Map()
+   private _validations = this.getValidations(
+      path.join(__dirname, 'validations', 'runtime'),
+   )
 
-  constructor(instance: NYXB, commandsDir: string, client: Client) {
-    this._instance = instance
-    this._commandsDir = commandsDir
-    this._slashCommands = new SlashCommands(client)
-    this._client = client
-    this._channelCommands = new ChannelCommands(instance)
-    this._customCommands = new CustomCommands(instance, this)
-    this._disabledCommands = new DisabledCommands(instance)
-    this._prefixes = new PrefixHandler(instance)
+   private _instance: NYXB
+   private _client: Client
+   private _commandsDir: string
+   private _slashCommands: SlashCommands
+   private _channelCommands: ChannelCommands
+   private _customCommands: CustomCommands
+   private _disabledCommands: DisabledCommands
+   private _prefixes: PrefixHandler
 
-    this._validations = [
-      ...this._validations,
-      ...this.getValidations(instance.validations?.runtime),
-    ]
+   constructor(instance: NYXB, commandsDir: string, client: Client) {
+      this._instance = instance
+      this._commandsDir = commandsDir
+      this._slashCommands = new SlashCommands(client)
+      this._client = client
+      this._channelCommands = new ChannelCommands(instance)
+      this._customCommands = new CustomCommands(instance, this)
+      this._disabledCommands = new DisabledCommands(instance)
+      this._prefixes = new PrefixHandler(instance)
 
-    this.readFiles()
-  }
+      this._validations = [
+         ...this._validations,
+         ...this.getValidations(instance.validations?.runtime),
+      ]
 
-  public get commands() {
-    return this._commands
-  }
+      this.readFiles()
+   }
 
-  public get channelCommands() {
-    return this._channelCommands
-  }
+   public get commands() {
+      return this._commands
+   }
 
-  public get slashCommands() {
-    return this._slashCommands
-  }
+   public get channelCommands() {
+      return this._channelCommands
+   }
 
-  public get customCommands() {
-    return this._customCommands
-  }
+   public get slashCommands() {
+      return this._slashCommands
+   }
 
-  public get disabledCommands() {
-    return this._disabledCommands
-  }
+   public get customCommands() {
+      return this._customCommands
+   }
 
-  public get prefixHandler() {
-    return this._prefixes
-  }
+   public get disabledCommands() {
+      return this._disabledCommands
+   }
 
-  private async readFiles() {
-    const defaultCommands = getAllFiles(path.join(__dirname, './commands'))
-    const files = getAllFiles(this._commandsDir)
-    const validations = [
-      ...this.getValidations(path.join(__dirname, 'validations', 'syntax')),
-      ...this.getValidations(this._instance.validations?.syntax),
-    ]
+   public get prefixHandler() {
+      return this._prefixes
+   }
 
-    for (let fileData of [...defaultCommands, ...files]) {
-      const { filePath } = fileData
-      const commandObject: CommandObject = fileData.fileContents
+   private async readFiles() {
+      const defaultCommands = getAllFiles(path.join(__dirname, './commands'))
+      const files = getAllFiles(this._commandsDir)
+      const validations = [
+         ...this.getValidations(path.join(__dirname, 'validations', 'syntax')),
+         ...this.getValidations(this._instance.validations?.syntax),
+      ]
 
-      const split = filePath.split(/[\/\\]/)
-      let commandName = split.pop()!
-      commandName = commandName.split('.')[0]
+      for (const fileData of [...defaultCommands, ...files]) {
+         const { filePath } = fileData
+         const commandObject: CommandObject = fileData.fileContents
 
-      const command = new Command(this._instance, commandName, commandObject)
+         const split = filePath.split(/[\/\\]/)
+         let commandName = split.pop()!
+         commandName = commandName.split('.')[0]
 
-      const {
-        description,
-        type,
-        testOnly,
-        delete: del,
-        aliases = [],
-        init = () => {},
-      } = commandObject
+         const command = new Command(this._instance, commandName, commandObject)
 
-      let defaultCommandValue: DefaultCommands | undefined
+         const {
+            description,
+            type,
+            testOnly,
+            delete: del,
+            aliases = [],
+            init = () => {},
+         } = commandObject
 
-      for (const [key, value] of Object.entries(DefaultCommands)) {
-        if (value === commandName.toLowerCase()) {
-          defaultCommandValue =
-            DefaultCommands[key as keyof typeof DefaultCommands]
-          break
-        }
-      }
+         let defaultCommandValue: DefaultCommands | undefined
 
-      if (
-        del ||
-        (defaultCommandValue &&
-          this._instance.disabledDefaultCommands.includes(defaultCommandValue))
-      ) {
-        if (type === 'SLASH' || type === 'BOTH') {
-          if (testOnly) {
-            for (const guildId of this._instance.testServers) {
-              this._slashCommands.delete(command.commandName, guildId)
+         for (const [key, value] of Object.entries(DefaultCommands)) {
+            if (value === commandName.toLowerCase()) {
+               defaultCommandValue
+            = DefaultCommands[key as keyof typeof DefaultCommands]
+               break
             }
-          } else {
-            this._slashCommands.delete(command.commandName)
-          }
-        }
+         }
 
-        continue
+         if (
+            del
+        || (defaultCommandValue
+          && this._instance.disabledDefaultCommands.includes(defaultCommandValue))
+         ) {
+            if (type === 'SLASH' || type === 'BOTH') {
+               if (testOnly) {
+                  for (const guildId of this._instance.testServers)
+                     this._slashCommands.delete(command.commandName, guildId)
+               }
+               else {
+                  this._slashCommands.delete(command.commandName)
+               }
+            }
+
+            continue
+         }
+
+         for (const validation of validations)
+            validation(command)
+
+         await init(this._client, this._instance)
+
+         const names = [command.commandName, ...aliases]
+
+         for (const name of names)
+            this._commands.set(name, command)
+
+         if (type === 'SLASH' || type === 'BOTH') {
+            const options
+          = commandObject.options
+          || this._slashCommands.createOptions(commandObject)
+
+            if (testOnly) {
+               for (const guildId of this._instance.testServers) {
+                  this._slashCommands.create(
+                     command.commandName,
+                     description!,
+                     options,
+                     guildId,
+                  )
+               }
+            }
+            else {
+               this._slashCommands.create(command.commandName, description!, options)
+            }
+         }
+      }
+   }
+
+   public async runCommand(
+      command: Command,
+      args: string[],
+      message: Message | null,
+      interaction: ChatInputCommandInteraction | null,
+   ) {
+      const { callback, type, cooldowns } = command.commandObject
+
+      if (message && type === CommandType.SLASH)
+         return
+
+      const guild = message ? message.guild : interaction?.guild
+      const member = (
+         message ? message.member : interaction?.member
+      ) as GuildMember
+      const user = message ? message.author : interaction?.user
+      const channel = (
+         message ? message.channel : interaction?.channel
+      ) as TextChannel
+
+      const usage: CommandUsage = {
+         client: command.instance.client,
+         instance: command.instance,
+         message,
+         interaction,
+         args,
+         text: args.join(' '),
+         guild,
+         member,
+         user: user!,
+         channel,
       }
 
-      for (const validation of validations) {
-        validation(command)
+      for (const validation of this._validations) {
+         if (!(await validation(command, usage, this._prefixes.get(guild?.id))))
+            return
       }
 
-      await init(this._client, this._instance)
+      if (cooldowns) {
+         const cooldownUsage: InternalCooldownConfig = {
+            cooldownType: cooldowns.type,
+            userId: user!.id,
+            actionId: `command_${command.commandName}`,
+            guildId: guild?.id,
+            duration: cooldowns.duration,
+            errorMessage: cooldowns.errorMessage,
+         }
 
-      const names = [command.commandName, ...aliases]
+         const result = this._instance.cooldowns?.canRunAction(cooldownUsage)
 
-      for (const name of names) {
-        this._commands.set(name, command)
+         if (typeof result === 'string')
+            return result
+
+         await this._instance.cooldowns?.start(cooldownUsage)
+
+         usage.cancelCooldown = () => {
+            this._instance.cooldowns?.cancelCooldown(cooldownUsage)
+         }
+
+         usage.updateCooldown = (expires: Date) => {
+            this._instance.cooldowns?.updateCooldown(cooldownUsage, expires)
+         }
       }
 
-      if (type === 'SLASH' || type === 'BOTH') {
-        const options =
-          commandObject.options ||
-          this._slashCommands.createOptions(commandObject)
+      return await callback(usage)
+   }
 
-        if (testOnly) {
-          for (const guildId of this._instance.testServers) {
-            this._slashCommands.create(
-              command.commandName,
-              description!,
-              options,
-              guildId
-            )
-          }
-        } else {
-          this._slashCommands.create(command.commandName, description!, options)
-        }
-      }
-    }
-  }
+   private getValidations(folder?: string) {
+      if (!folder)
+         return []
 
-  public async runCommand(
-    command: Command,
-    args: string[],
-    message: Message | null,
-    interaction: ChatInputCommandInteraction | null
-  ) {
-    const { callback, type, cooldowns } = command.commandObject
-
-    if (message && type === CommandType.SLASH) {
-      return
-    }
-
-    const guild = message ? message.guild : interaction?.guild
-    const member = (
-      message ? message.member : interaction?.member
-    ) as GuildMember
-    const user = message ? message.author : interaction?.user
-    const channel = (
-      message ? message.channel : interaction?.channel
-    ) as TextChannel
-
-    const usage: CommandUsage = {
-      client: command.instance.client,
-      instance: command.instance,
-      message,
-      interaction,
-      args,
-      text: args.join(' '),
-      guild,
-      member,
-      user: user!,
-      channel,
-    }
-
-    for (const validation of this._validations) {
-      if (!(await validation(command, usage, this._prefixes.get(guild?.id)))) {
-        return
-      }
-    }
-
-    if (cooldowns) {
-      const cooldownUsage: InternalCooldownConfig = {
-        cooldownType: cooldowns.type,
-        userId: user!.id,
-        actionId: `command_${command.commandName}`,
-        guildId: guild?.id,
-        duration: cooldowns.duration,
-        errorMessage: cooldowns.errorMessage,
-      }
-
-      const result = this._instance.cooldowns?.canRunAction(cooldownUsage)
-
-      if (typeof result === 'string') {
-        return result
-      }
-
-      await this._instance.cooldowns?.start(cooldownUsage)
-
-      usage.cancelCooldown = () => {
-        this._instance.cooldowns?.cancelCooldown(cooldownUsage)
-      }
-
-      usage.updateCooldown = (expires: Date) => {
-        this._instance.cooldowns?.updateCooldown(cooldownUsage, expires)
-      }
-    }
-
-    return await callback(usage)
-  }
-
-  private getValidations(folder?: string) {
-    if (!folder) {
-      return []
-    }
-
-    return getAllFiles(folder).map((fileData) => fileData.fileContents)
-  }
+      return getAllFiles(folder).map(fileData => fileData.fileContents)
+   }
 }
 
 export default CommandHandler
--- src/command-handler/CustomCommands.ts
+++ src/command-handler/CustomCommands.ts
@@ -1,123 +1,116 @@
-import { CommandInteraction, Message } from 'discord.js'
+import type { CommandInteraction, Message } from 'discord.js'
 
 import customCommandSchema from '../models/custom-command-schema'
-import CommandHandler from './CommandHandler'
-import NYXB from '../../typings'
+import type NYXB from '../../typings'
+import type CommandHandler from './CommandHandler'
 
 class CustomCommands {
-  // guildId-commandName: response
-  private _customCommands = new Map()
-  private _commandHandler: CommandHandler
-  private _instance: NYXB
+   // guildId-commandName: response
+   private _customCommands = new Map()
+   private _commandHandler: CommandHandler
+   private _instance: NYXB
 
-  constructor(instance: NYXB, commandHandler: CommandHandler) {
-    this._instance = instance
-    this._commandHandler = commandHandler
+   constructor(instance: NYXB, commandHandler: CommandHandler) {
+      this._instance = instance
+      this._commandHandler = commandHandler
 
-    this.loadCommands()
-  }
+      this.loadCommands()
+   }
 
-  async loadCommands() {
-    if (!this._instance.isConnectedToDB) {
-      return
-    }
+   async loadCommands() {
+      if (!this._instance.isConnectedToDB)
+         return
 
-    const results = await customCommandSchema.find({})
+      const results = await customCommandSchema.find({})
 
-    for (const result of results) {
-      const { _id, response } = result
+      for (const result of results) {
+         const { _id, response } = result
+         this._customCommands.set(_id, response)
+      }
+   }
+
+   getCommands(guildId: string) {
+      const commands = []
+
+      for (const [key] of this._customCommands) {
+         const [id, commandName] = key.split('-')
+         if (id === guildId)
+            commands.push(commandName)
+      }
+
+      return commands
+   }
+
+   async create(
+      guildId: string,
+      commandName: string,
+      description: string,
+      response: string,
+   ) {
+      if (!this._instance.isConnectedToDB)
+         return
+
+      const _id = `${guildId}-${commandName}`
+
       this._customCommands.set(_id, response)
-    }
-  }
 
-  getCommands(guildId: string) {
-    const commands = []
+      this._commandHandler.slashCommands.create(
+         commandName,
+         description,
+         [],
+         guildId,
+      )
 
-    for (const [key] of this._customCommands) {
-      const [id, commandName] = key.split('-')
-      if (id === guildId) {
-        commands.push(commandName)
-      }
-    }
+      await customCommandSchema.findOneAndUpdate(
+         {
+            _id,
+         },
+         {
+            _id,
+            response,
+         },
+         {
+            upsert: true,
+         },
+      )
+   }
 
-    return commands
-  }
+   async delete(guildId: string, commandName: string) {
+      if (!this._instance.isConnectedToDB)
+         return
 
-  async create(
-    guildId: string,
-    commandName: string,
-    description: string,
-    response: string
-  ) {
-    if (!this._instance.isConnectedToDB) {
-      return
-    }
+      const _id = `${guildId}-${commandName}`
 
-    const _id = `${guildId}-${commandName}`
+      this._customCommands.delete(_id)
 
-    this._customCommands.set(_id, response)
+      this._commandHandler.slashCommands.delete(commandName, guildId)
 
-    this._commandHandler.slashCommands.create(
-      commandName,
-      description,
-      [],
-      guildId
-    )
+      await customCommandSchema.deleteOne({ _id })
+   }
 
-    await customCommandSchema.findOneAndUpdate(
-      {
-        _id,
-      },
-      {
-        _id,
-        response,
-      },
-      {
-        upsert: true,
-      }
-    )
-  }
+   async run(
+      commandName: string,
+      message: Message | null,
+      interaction: CommandInteraction | null,
+   ) {
+      if (!message && !interaction)
+         return
 
-  async delete(guildId: string, commandName: string) {
-    if (!this._instance.isConnectedToDB) {
-      return
-    }
+      const guild = message ? message.guild : interaction!.guild
+      if (!guild)
+         return
 
-    const _id = `${guildId}-${commandName}`
+      const _id = `${guild.id}-${commandName}`
+      const response = this._customCommands.get(_id)
+      if (!response)
+         return
 
-    this._customCommands.delete(_id)
+      if (message)
+         message.channel.send(response).catch(() => {})
 
-    this._commandHandler.slashCommands.delete(commandName, guildId)
-
-    await customCommandSchema.deleteOne({ _id })
-  }
-
-  async run(
-    commandName: string,
-    message: Message | null,
-    interaction: CommandInteraction | null
-  ) {
-    if (!message && !interaction) {
-      return
-    }
-
-    const guild = message ? message.guild : interaction!.guild
-    if (!guild) {
-      return
-    }
-
-    const _id = `${guild.id}-${commandName}`
-    const response = this._customCommands.get(_id)
-    if (!response) {
-      return
-    }
-
-    if (message) {
-      message.channel.send(response).catch(() => {})
-    } else if (interaction) {
-      interaction.reply(response).catch(() => {})
-    }
-  }
+      else if (interaction)
+         interaction.reply(response).catch(() => {})
+   }
 }
 
 export default CustomCommands
