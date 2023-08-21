7ee6ce9c8d58a73d87e2ca50a136af9bd3790975
nyxb <contact@nyxb.xyz>
2023-08-21 22:13:43 +0200

    🐛 fix(owner-only.ts): fix indentation and remove unnecessary semicolons for better code readability

--- src/command-handler/validations/runtime/owner-only.ts
+++ src/command-handler/validations/runtime/owner-only.ts
@@ -1,15 +1,14 @@
-import Command from "../../Command";
-import { CommandUsage } from "../../../../typings";
+import type Command from '../../Command'
+import type { CommandUsage } from '../../../../typings'
 
 export default (command: Command, usage: CommandUsage) => {
-  const { instance, commandObject } = command;
-  const { botOwners } = instance;
-  const { ownerOnly } = commandObject;
-  const { user } = usage;
+   const { instance, commandObject } = command
+   const { botOwners } = instance
+   const { ownerOnly } = commandObject
+   const { user } = usage
 
-  if (ownerOnly === true && !botOwners.includes(user!.id)) {
-    return false;
-  }
+   if (ownerOnly === true && !botOwners.includes(user!.id))
+      return false
 
-  return true;
-};
+   return true
+}
