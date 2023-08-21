d8e101ad644d555979c2bfb92afd1ef0d7f73d38
nyxb <contact@nyxb.xyz>
2023-08-21 22:15:45 +0200

    🐛 fix(test-only.ts): improve code formatting and remove unnecessary semicolons for better readability and consistency

--- src/command-handler/validations/runtime/test-only.ts
+++ src/command-handler/validations/runtime/test-only.ts
@@ -1,13 +1,12 @@
-import Command from "../../Command";
-import { CommandUsage } from "../../../../typings";
+import type Command from '../../Command'
+import type { CommandUsage } from '../../../../typings'
 
 export default (command: Command, usage: CommandUsage) => {
-  const { instance, commandObject } = command;
-  const { guild } = usage;
+   const { instance, commandObject } = command
+   const { guild } = usage
 
-  if (commandObject.testOnly !== true) {
-    return true;
-  }
+   if (commandObject.testOnly !== true)
+      return true
 
-  return instance.testServers.includes(guild?.id || "");
-};
+   return instance.testServers.includes(guild?.id || '')
+}
